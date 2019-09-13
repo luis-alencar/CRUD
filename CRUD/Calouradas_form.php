83f8015942d43717cc0ac88e0dfbaff83150811f
luis-alencar <luisfernando.fa@hotmail.com>
2019-09-13 16:21:14 -0300

    m

--- CRUD/Calouradas_form.php
+++ CRUD/Calouradas_form.php
@@ -3,42 +3,48 @@ require_once("config.php");
 require_once("Calouradas_funcoes.php");
 
 ?>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><link rel="stylesheet" href="style.css">
 <FORM ACTION="Calouradas_salvar.php" METHOD="POST">
-	<strong>Formulário de Calouradas</strong><br/>
-	<div class="container"> 
-	<div class="imagem_form row col-sm-2">
+
+<div class="container"></div>
+<input id="btn-back" type="button" value="Voltar" onClick="history.go(-1)"> 
+
+<strong>Formulário de Calouradas<BR/></strong><br/>
+</div>
+<div class="container">
+	<div class="radio-group">
 		<input type="radio" name="imagem" id="i1" />
-		<label for="i1"><img src="https://cdn.dribbble.com/users/3689326/screenshots/6599423/1.jpg" alt=""></label>
+		<label class="radio" id="imagem_form" for="i1"><img src="img/mitologica.png" alt=""></label>
 		<input type="radio" name="imagem" id="i2" />
-		<label for="i2"><img src="img/civilizada.jpg" alt=""></label>
+		<label class="radio" id="imagem_form" for="i2"><img src="img/sistematica.jpg" alt=""></label>
 		<input type="radio" name="imagem" id="i3" />
-		<label for="i3"><img src="img/sistematica.jpg" alt=""></label>
-	</div>	
-		</div>	
+		<label class="radio" id="imagem_form" for="i3"><img src="img/civilizada.jpg" alt=""></label>
+	</div>
+</div>
 	<?=get_mensagem($mensagem)?>
-	<div class="container"> 
-	<div class="row">
-	<INPUT TYPE="hidden" NAME="codigo_livro" VALUE="<?=$dados['codigo_livro']?>">
+	<INPUT TYPE="date" NAME="DATA" VALUE="<?=$dados['Data']?>">
 	
 	<BR/>Atletica:<BR/>
-	<INPUT TYPE="text" NAME="autor" VALUE="<?=$dados['Atletica']?>">
+	<INPUT TYPE="text" NAME="TEMA" VALUE="<?=$dados['Tema']?>">
 	
 	<BR/>Tema:<BR/>
-	<INPUT TYPE="TEXT" NAME="titulo" VALUE="<?=$dados['Tema']?>">
+	<INPUT TYPE="TEXT" NAME="Hora" VALUE="<?=$dados['Hora']?>">
 	
+	<BR/>Tema:<BR/>
+	<INPUT TYPE="TEXT" NAME="Preço" VALUE="<?=$dados['Preco']?>">
+	
+
+
 	<BR/>Data: <?=$dados['Data']?><BR/>
 	<SELECT NAME="id_Calouradas">
-		<option value="">Selecione uma Atletica</option>
+		<option value="Date">Escolha uma Data</option>
 		<?php
 		//echo "teste2:".print_r($bd,1).":";
-		$result_editora = editora_listar_todos($bd);
+		$result_Calouradas = Calouradas_listar_todos($bd);
 		
-		while( $dados_editora = editora_anexar_resultado($result_editora) ){ ?>
-			<OPTION VALUE=<?=$dados_editora['id']?> <?=select_get_selected($dados_editora['id'],$dados['editora_id'])?> >
-				<?=$dados_editora['nome']?>
+		while( $dados_Calouradas = Calouradas_anexar_resultado($result_Calouradas) ){ ?>
+			<OPTION VALUE=<?=$dados_Calouradas['id']?> <?=select_get_selected($dados_Calouradas['id'],$dados['Calouradas_id'])?> >
+				<?=$dados_Calouradas['nome']?>
 			</OPTION>
 		<?php 
 		} 
@@ -47,9 +53,8 @@ require_once("Calouradas_funcoes.php");
 	
 	
 	<BR/>Resumo:<BR/>
-	<textarea NAME='resumo'><?=$dados['resumo']?></textarea>
+	<textarea NAME='resumo'><?=$dados['resumo']?></textarea><br/><br/>
 	
 	<INPUT TYPE="submit" NAME="btn_acao" VALUE="Enviar">
-	</div>
-	</div> 
+
 </FORM>
